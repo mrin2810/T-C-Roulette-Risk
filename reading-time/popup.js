9e706f908d555298028e531d6f26f47caf034d1e
Saquib Khan <ksaquib54@gmail.com>
2023-09-24 11:12:30 -0400

    added style

--- reading-time/popup.js
+++ reading-time/popup.js
@@ -1,58 +1,3 @@
-// const requestConfig = {
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//   };
-  
-//   const currentDate = new Date().toISOString().slice(0, 10);
-//   console.log(currentDate); // output: 2023-04-06
-  
-// document.addEventListener('DOMContentLoaded', function() {
-//     chrome.storage.sync.get(["data"], (result) => {
-//         const data = result["data"] || {};
-//         document.getElementById('history').addEventListener('click', function() {
-//         chrome.tabs.create({ url: chrome.runtime.getURL('data.html') });
-//         });
-//         const summaryButton = document.getElementById('highlights');
-//         const saveButton = document.getElementById('save');
-    
-//         summaryButton.addEventListener('click', function() {
-//             const highlightedData = document.querySelector("#highlighted-data");
-//             const summaryData = document.querySelector('#summary').value;
-//             axios.post(
-//                 "https://uxb6df64xcgfe7d754aw5k5lyu0giion.lambda-url.us-east-1.on.aws/",
-//                 summaryData,
-//                 requestConfig
-//               )
-//               .then((res) => {
-//                 highlightedData.innerHTML = res.data;
-//                 console.log(`Status: ${res.status}`);
-//                 console.log("Body: ", res.data);
-//               })
-//               .catch((err) => {
-//                 // console.log(err);
-//                 console.error(err.response.status);
-//                 console.error(err.response.statusText);
-//                 console.error(err.code);
-//               });
-            
-//             // const inputData = document.querySelector('#summary').value;
-//         // Make API call with inputData
-//         // Handle response from API
-//         });
-    
-//         saveButton.addEventListener('click', function() {
-//         const companyName = document.querySelector('#company-name').value;
-//         const summaryData = document.querySelector('#summary').value;
-//         data[companyName]= summaryData;
-//         chrome.storage.sync.set({"data": data}, function() {
-//             console.log("Data Saved")
-//         });
-//         });
-
-//     });
-//   });
-
 var myHeaders = new Headers();
 myHeaders.append("Accept", "application/json");
 // myHeaders.append("Access-Control-Allow-Origin", "*");
@@ -115,13 +60,13 @@ document.addEventListener('DOMContentLoaded', function() {
             .then((result) => {
                 result = JSON.parse(result.body);
                 const negativePoints = result["Negative Response"].split('\n-');
-                let formattedNegative = 'Suspicious Comments: \n\n';
+                let formattedNegative = '<h4>Suspicious Comments:</h4> \n\n';
                 for(let i = 1; i < negativePoints.length; i++) {
                     formattedNegative += `<div class="negative-comment"> ${i}: ${negativePoints[i]}</div>`;
                 }
                 
                 const positivePoints = result["Positive Response"].split('\n-');
-                let formattedPositive = 'Positive Comments: \n\n';
+                let formattedPositive = '<h4>Positive Comments:</h4>\n\n';
                 for(let i = 1; i < positivePoints.length; i++) {
                     formattedPositive += `<div class="positive-comment">${i}: ${positivePoints[i]}</div>`;
                 }
